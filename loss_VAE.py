90dd8e2a376c4002ff7624802f1acd00527106f1
wyzjack <18362146969@163.com>
2019-10-07 20:17:04 +0800

    ubuntu commit 1

---
+++ loss_VAE.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Power by Zongsheng Yue 2019-03-20 19:48:14
+
+import torch
+#import torch.nn.functional as F
+
+lamX = [0.2,0.2,0.2,1]
+lamE = [0.2,0.2,0.2,1]
+lamReY=0.05
+lamReZ=0.005
+
+
+def loss_fn(Xlist, Zlist,Elist,reYlist,reZlist , X,Y,batch_size):
+    lossX=0
+    lossE=0
+    lossReY=0
+    lossReZ=0
+    for i in range(len(Xlist)-1):
+        if(i==len(Xlist)-2):
+            lossOut = lamX[i]*torch.nn.functional.mse_loss(Xlist[i+1], X,size_average=False,reduce=True).div_(2)
+            lossX+=lossOut
+        else:
+            lossX += lamX[i]*torch.nn.functional.mse_loss(Xlist[i+1], X,size_average=False,reduce=True).div_(2)
+        lossE += lamE[i]*torch.nn.functional.mse_loss(Elist[i], X-Xlist[i],size_average=False,reduce=True).div_(2)
+        lossReY += lamReY*torch.nn.functional.mse_loss(reYlist[i], Xlist[i],size_average=False,reduce=True).div_(2)
+        lossReZ += lamReZ*torch.nn.functional.mse_loss(reZlist[i], Zlist[i],size_average=False,reduce=True).div_(2)
+    loss = lossX+lossE+lossReY+lossReZ
+    return loss/batch_size,lossOut/batch_size,lossX/batch_size,lossE/batch_size,lossReY/batch_size,lossReZ/batch_size
--- train_simulation.py
+++ train_simulation.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 # Power by Zongsheng Yue 2019-01-10 22:41:49
 
-#from glob import glob
+from glob import glob
 import warnings
 import time
 import numpy as np
@@ -11,8 +11,8 @@ import torchvision.utils as vutils
 from utils import batch_PSNR, batch_SSIM
 from tensorboardX import SummaryWriter
 from math import ceil
-from loss_VAE import loss_fn
-from networks import VAENet
+from loss import loss_fn
+from networks import VDN
 from datasets import DenoisingDatasets
 import torch.optim as optim
 import torch.nn.functional as F
@@ -21,7 +21,7 @@ import torch
 import os
 import sys
 from pathlib import Path
-from options_VAE import set_opts
+from options import set_opts
 import matplotlib.pyplot as plt
 
 # filter warnings
@@ -46,8 +46,8 @@ def imshow(X):
 
 def train_model(net, datasets, optimizer, lr_scheduler, criterion):
     C = args.chn
-    clip_grad = args.clip_grad
-#    clip_grad_S = args.clip_grad_S
+    clip_grad_D = args.clip_grad_D
+    clip_grad_S = args.clip_grad_S
     batch_size = {'train': args.batch_size, 'test_cbsd681': 1, 'test_cbsd682': 1, 'test_cbsd683': 1}
     data_loader = {phase: torch.utils.data.DataLoader(datasets[phase], batch_size=batch_size[phase],
           shuffle=True, num_workers=args.num_workers, pin_memory=True) for phase in datasets.keys()}
@@ -60,11 +60,12 @@ def train_model(net, datasets, optimizer, lr_scheduler, criterion):
     else:
         step = 0
         step_img = {x: 0 for x in _modes}
-    param = [x for name,x in net.named_parameters()]
+    param_D = [x for name, x in net.named_parameters() if 'dnet' in name.lower()]
+    param_S = [x for name, x in net.named_parameters() if 'snet' in name.lower()]
     for epoch in range(args.epoch_start, args.epochs):
-        loss_per_epoch = {x: 0 for x in ['Loss', 'Loss_Out', 'Loss_X', 'Loss_E','Loss_ReY','Loss_ReZ']}
+        loss_per_epoch = {x: 0 for x in ['Loss', 'lh', 'KLG', 'KLIG']}
         mse_per_epoch = {x: 0 for x in _modes}
-        grad_norm= 0
+        grad_norm_D = grad_norm_S = 0
         tic = time.time()
         # train stage
         net.train()
@@ -75,36 +76,43 @@ def train_model(net, datasets, optimizer, lr_scheduler, criterion):
         for ii, data in enumerate(data_loader[phase]):
             im_noisy, im_gt, sigmaMapEst, sigmaMapGt = [x.cuda() for x in data]
             optimizer.zero_grad()
-            Xlist,Zlist,Elist,reYlist,reZlist = net(im_noisy)
-            loss,lossOut,lossX,lossE,lossReY,lossReZ = criterion(Xlist, Zlist,Elist,reYlist,reZlist , im_gt,im_noisy,args.batch_size)
-            
+            phi_Z, phi_sigma = net(im_noisy, 'train')
+            loss, g_lh, kl_g, kl_Igam = criterion(phi_Z, phi_sigma, im_noisy, im_gt,
+                                                          sigmaMapGt, args.eps2, radius=args.radius)
             loss.backward()
             # clip the gradnorm
-            total_norm = nn.utils.clip_grad_norm_(param, clip_grad)
-            grad_norm = (grad_norm*(ii/(ii+1)) + total_norm/(ii+1))
+            total_norm_D = nn.utils.clip_grad_norm_(param_D, clip_grad_D)
+            total_norm_S = nn.utils.clip_grad_norm_(param_S, clip_grad_S)
+            grad_norm_D = (grad_norm_D*(ii/(ii+1)) + total_norm_D/(ii+1))
+            grad_norm_S = (grad_norm_S*(ii/(ii+1)) + total_norm_S/(ii+1))
             optimizer.step()
 
             loss_per_epoch['Loss'] += loss.item() / num_iter_epoch[phase]
-            loss_per_epoch['Loss_Out'] += lossOut.item() / num_iter_epoch[phase]
-            loss_per_epoch['Loss_X'] += lossX.item() / num_iter_epoch[phase]
-            loss_per_epoch['Loss_E'] += lossE.item() / num_iter_epoch[phase]
-            loss_per_epoch['Loss_ReY'] += lossReY.item() / num_iter_epoch[phase]
-            loss_per_epoch['Loss_ReZ'] += lossReZ.item() / num_iter_epoch[phase]
-            #im_denoise = torch.clamp(Xlist[-1].detach().data, 0.0, 1.0) #####        
-
+            loss_per_epoch['lh'] += g_lh.item() / num_iter_epoch[phase]
+            loss_per_epoch['KLG'] += kl_g.item() / num_iter_epoch[phase]
+            loss_per_epoch['KLIG'] += kl_Igam.item() / num_iter_epoch[phase]
+            im_denoise = torch.clamp(im_noisy-phi_Z[:, :C, ].detach().data, 0.0, 1.0)
+            mse = F.mse_loss(im_denoise, im_gt)
+            mse_per_epoch[phase] += mse
+            
+            
             if (ii+1) % args.print_freq == 0:
-                log_str = '[Epoch:{:>2d}/{:<2d}] {:s}:{:0>4d}/{:0>4d}, ' + \
-                        'loss={:.4f}, Out={:.4f}, X={:.4f}, E={:.4f}, ReY={:.4f}, ReZ={:.4f}, GNorm:{:.1e}/{:.1e}, lr={:.1e}'
+                log_str = '[Epoch:{:>2d}/{:<2d}] {:s}:{:0>4d}/{:0>4d}, lh={:+4.2f}, ' + \
+                        'KLG={:+>7.2f}, KLIG={:+>6.2f}, mse={:.2e}, GNorm_D:{:.1e}/{:.1e}, ' + \
+                                                                  'GNorm_S:{:.1e}/{:.1e}, lr={:.1e}'
                 print(log_str.format(epoch+1, args.epochs, phase, ii+1, num_iter_epoch[phase],
-                                         loss.item(), lossOut.item(), lossX.item(),lossE.item(),lossReY.item(),
-                                         lossReZ.item(), clip_grad,total_norm, lr))
+                                         g_lh.item(), kl_g.item(), kl_Igam.item(), mse, clip_grad_D,
+                                                       total_norm_D, clip_grad_S, total_norm_S, lr))
                 writer.add_scalar('Train Loss Iter', loss.item(), step)
-                writer.add_scalar('Gradient Norm Iter', total_norm, step)
+                writer.add_scalar('Train MSE Iter', mse, step)
+                writer.add_scalar('Gradient Norm_D Iter', total_norm_D, step)
+                writer.add_scalar('Gradient Norm_S Iter', total_norm_S, step)
                 step += 1
                 #plot
                 show1 = im_gt.cpu().detach().numpy()[0].transpose((1,2,0))
+                #print(show1)
                 show2 = im_noisy.cpu().detach().numpy()[0].transpose((1,2,0))
-                show3 = Xlist[-1].cpu().detach().numpy()[0].transpose((1,2,0))
+                show3 = im_denoise.cpu().detach().numpy()[0].transpose((1,2,0))
                 #show4 = phi_sigma.cpu().detach().numpy()[0]
                 #show5 = sigmaMapGt.cpu().detach().numpy()[0]
                 #show6 = sigmaMapEst.cpu().detach().numpy()[0]
@@ -114,10 +122,33 @@ def train_model(net, datasets, optimizer, lr_scheduler, criterion):
                 #toshow = np.vstack((toshow1,toshow2))
                 imshow(toshow1)
                 
+            if (ii+1) % (20*args.print_freq) == 0:
+                alpha = torch.exp(phi_sigma[:, :C, ])
+                beta = torch.exp(phi_sigma[:, C:, ])
+                sigmaMap_pred = beta / (alpha-1)
+                x1 = vutils.make_grid(im_denoise, normalize=True, scale_each=True)
+                writer.add_image(phase+' Denoised images', x1, step_img[phase])
+                x2 = vutils.make_grid(im_gt, normalize=True, scale_each=True)
+                writer.add_image(phase+' GroundTruth', x2, step_img[phase])
+                x3 = vutils.make_grid(sigmaMap_pred, normalize=True, scale_each=True)
+                writer.add_image(phase+' Predict Sigma', x3, step_img[phase])
+                x4 = vutils.make_grid(sigmaMapGt, normalize=True, scale_each=True)
+                writer.add_image(phase+' Groundtruth Sigma', x4, step_img[phase])
+                x5 = vutils.make_grid(im_noisy, normalize=True, scale_each=True)
+                writer.add_image(phase+' Noisy Image', x5, step_img[phase])
+                step_img[phase] += 1
 
+        mse_per_epoch[phase] /= (ii+1)
+        log_str ='{:s}: Loss={:+.2e}, lh={:+.2e}, KL_Guass={:+.2e}, KLIG={:+.2e}, mse={:.3e}, ' + \
+                                                      'GNorm_D={:.1e}/{:.1e}, GNorm_S={:.1e}/{:.1e}'
+        print(log_str.format(phase, loss_per_epoch['Loss'], loss_per_epoch['lh'],
+                                loss_per_epoch['KLG'], loss_per_epoch['KLIG'], mse_per_epoch[phase],
+                                                clip_grad_D, grad_norm_D, clip_grad_S, grad_norm_S))
         writer.add_scalar('Loss_epoch', loss_per_epoch['Loss'], epoch)
-        writer.add_scalar('Mean Grad Norm epoch', grad_norm, epoch)
-        clip_grad = min(clip_grad, grad_norm)
+        writer.add_scalar('Mean Grad Norm_D epoch', grad_norm_D, epoch)
+        writer.add_scalar('Mean Grad Norm_S epoch', grad_norm_S, epoch)
+        clip_grad_D = min(clip_grad_D, grad_norm_D)
+        clip_grad_S = min(clip_grad_S, grad_norm_S)
         print('-'*150)
 
         # test stage
@@ -128,9 +159,9 @@ def train_model(net, datasets, optimizer, lr_scheduler, criterion):
             for ii, data in enumerate(data_loader[phase]):
                 im_noisy, im_gt = [x.cuda() for x in data]
                 with torch.set_grad_enabled(False):
-                    Xlist,Zlist,Elist,reYlist,reZlist = net(im_noisy)
+                    phi_Z, phi_sigma = net(im_noisy, 'train')
 
-                im_denoise = torch.clamp(Xlist[-1].detach().data, 0.0, 1.0)
+                im_denoise = torch.clamp(im_noisy-phi_Z[:, :C, ].detach().data, 0.0, 1.0)
                 mse = F.mse_loss(im_denoise, im_gt)
                 mse_per_epoch[phase] += mse
                 psnr_iter = batch_PSNR(im_denoise, im_gt)
@@ -144,15 +175,15 @@ def train_model(net, datasets, optimizer, lr_scheduler, criterion):
                     print(log_str.format(epoch+1, args.epochs, phase, ii+1, num_iter_epoch[phase],
                                                                          mse, psnr_iter, ssim_iter))
                 # tensorboardX summary
-#                    alpha = torch.exp(phi_sigma[:, :C, ])
-#                    beta = torch.exp(phi_sigma[:, C:, ])
-#                    sigmaMap_pred = beta / (alpha-1)
+                    alpha = torch.exp(phi_sigma[:, :C, ])
+                    beta = torch.exp(phi_sigma[:, C:, ])
+                    sigmaMap_pred = beta / (alpha-1)
                     x1 = vutils.make_grid(im_denoise, normalize=True, scale_each=True)
                     writer.add_image(phase+' Denoised images', x1, step_img[phase])
                     x2 = vutils.make_grid(im_gt, normalize=True, scale_each=True)
                     writer.add_image(phase+' GroundTruth', x2, step_img[phase])
-#                    x3 = vutils.make_grid(sigmaMap_pred, normalize=True, scale_each=True)
-#                    writer.add_image(phase+' Predict Sigma', x3, step_img[phase])
+                    x3 = vutils.make_grid(sigmaMap_pred, normalize=True, scale_each=True)
+                    writer.add_image(phase+' Predict Sigma', x3, step_img[phase])
                     x4 = vutils.make_grid(im_noisy, normalize=True, scale_each=True)
                     writer.add_image(phase+' Noise Image', x4, step_img[phase])
                     step_img[phase] += 1
@@ -175,7 +206,8 @@ def train_model(net, datasets, optimizer, lr_scheduler, criterion):
                 'epoch': epoch+1,
                 'step': step+1,
                 'step_img': {x: step_img[x] for x in _modes},
-                'grad_norm': clip_grad,
+                'grad_norm_D': clip_grad_D,
+                'grad_norm_S': clip_grad_S,
                 'model_state_dict': net.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 'lr_scheduler_state_dict': lr_scheduler.state_dict()
@@ -194,7 +226,8 @@ def train_model(net, datasets, optimizer, lr_scheduler, criterion):
 
 def main():
     # build the model
-    net = VAENet.VAEnet()
+    net = VDN.VDNU(args.chn, activation=args.activation, act_init=args.relu_init, wf=args.wf,
+                                                                            batch_norm=args.bn_UNet)
     # move the model to GPU
     net = nn.DataParallel(net).cuda()
 
